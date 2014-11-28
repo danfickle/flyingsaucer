1e2f49d3a0af8f01f6620571239c235aca1cf414
Dan Fickle <newtab@gmail.com>
2014-11-28 18:28:51 +1100

    Added a continuous renderer and usage function.

---
+++ flying-saucer-core/src/main/java/org/xhtmlrenderer/renderers/ContinuousRenderer.java
@@ -0,0 +1,183 @@
+package org.xhtmlrenderer.renderers;
+
+import java.awt.Rectangle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xhtmlrenderer.displaylist.DlOutputDevice;
+import org.xhtmlrenderer.displaylist.DlTextRenderer;
+import org.xhtmlrenderer.layout.BoxBuilder;
+import org.xhtmlrenderer.layout.Layer;
+import org.xhtmlrenderer.layout.LayoutContext;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.render.BlockBox;
+import org.xhtmlrenderer.render.Box;
+import org.xhtmlrenderer.render.RenderingContext;
+import org.xhtmlrenderer.render.ViewportBox;
+import org.xhtmlrenderer.resource.ResourceLoadHelper;
+import org.xhtmlrenderer.simple.HtmlNamespaceHandler;
+import com.github.neoflyingsaucer.displaylist.DisplayListImpl;
+import com.github.neoflyingsaucer.extend.output.DisplayList;
+import com.github.neoflyingsaucer.extend.output.FontContext;
+import com.github.neoflyingsaucer.extend.output.FontResolver;
+import com.github.neoflyingsaucer.extend.output.ImageResolver;
+import com.github.neoflyingsaucer.extend.output.ReplacedElementResolver;
+import com.github.neoflyingsaucer.extend.useragent.HTMLResourceI;
+import com.github.neoflyingsaucer.extend.useragent.UserAgentCallback;
+
+public class ContinuousRenderer 
+{
+	private Box rootBox;
+	private Document doc;
+	private String uri;
+	private FontContext fontContext;
+	private Rectangle viewportSize;
+
+	private final UserAgentCallback cb;
+	private final SharedContext sharedContext;
+	private final DisplayList displayList;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ContinuousRenderer.class);
+	
+	public ContinuousRenderer(UserAgentCallback cb)
+	{
+		this.cb = cb;
+		this.sharedContext = newSharedContext(cb);
+		this.displayList = new DisplayListImpl();
+	}
+	
+	public void setDocumentUri(String uri)
+	{
+		this.uri = uri;
+	}
+
+	public void setImageResolver(ImageResolver imgResolver)
+	{
+		sharedContext.setImageResolver(imgResolver);
+	}
+
+	public void setFontResolver(FontResolver fontResolver)
+	{
+		sharedContext.setFontResolver(fontResolver);
+	}
+	
+	public void setReplacedElementResolver(ReplacedElementResolver resolver)
+	{
+		sharedContext.setReplacedElementResolver(resolver);
+		sharedContext.setReplacedElementFactory(null);
+	}
+	
+	public void setFontContext(FontContext ctx)
+	{
+		fontContext = ctx;
+	}
+	
+	public void setViewportSize(int width, int height)
+	{
+		viewportSize = new Rectangle(width, height);
+	}
+	
+	public SharedContext getSharedContext()
+	{
+        return sharedContext;
+    }
+	
+	public DisplayList renderToList()
+	{
+		HTMLResourceI res = ResourceLoadHelper.loadHtmlDocument(uri, cb);
+		sharedContext.setDocumentURI(res.getURI());
+		doc = res.getDocument();
+		
+		print();
+		return displayList;
+	}
+	
+    private void doDocumentLayout() 
+    {
+            if (doc == null) 
+            	return;
+
+            LayoutContext c = newLayoutContext();
+
+            long start = System.currentTimeMillis();
+
+            BlockBox root = (BlockBox) getRootBox();
+
+            if (root != null) {
+                root.reset(c);
+            } else {
+                root = BoxBuilder.createRootBox(c, doc);
+                setRootBox(root);
+            }
+
+            sharedContext.set_TempCanvas(viewportSize);
+            root.setContainingBlock(new ViewportBox(viewportSize));
+            root.layout(c);
+
+            long end = System.currentTimeMillis();
+            
+            LOGGER.info("Layout took " + (end - start) + " milliseconds");
+    }
+
+    private SharedContext newSharedContext(final UserAgentCallback userAgent) 
+    {
+    	SharedContext context = new SharedContext(userAgent);
+        context.setTextRenderer(new DlTextRenderer());
+        return context;
+    }
+
+    private LayoutContext newLayoutContext()
+    {
+        LayoutContext result = getSharedContext().newLayoutContextInstance();
+        result.setFontContext(fontContext);
+        getSharedContext().getTextRenderer().setup(result.getFontContext());
+        return result;
+    }
+    
+    private RenderingContext newRenderingContext() 
+    {
+        RenderingContext result = getSharedContext().newRenderingContextInstance();
+        result.setFontContext(fontContext);
+        result.setOutputDevice(new DlOutputDevice(this.displayList));
+
+        getSharedContext().getTextRenderer().setup(result.getFontContext());
+
+        final Box rb = getRootBox();
+
+        if (rb != null) 
+            result.setRootLayer(rb.getLayer());
+
+        return result;
+    }
+	
+    private void print() 
+    {
+            getSharedContext().setPrint(false);
+            getSharedContext().setDPI(72f);
+            getSharedContext().setUserAgentCallback(this.cb);
+            getSharedContext().setNamespaceHandler(new HtmlNamespaceHandler());
+            getSharedContext().getCss().setDocumentContext(getSharedContext(), getSharedContext().getNamespaceHandler(), doc);
+
+            doDocumentLayout();
+            
+            RenderingContext c = newRenderingContext();
+            
+            getRootLayer().paint(c);
+    }
+    
+    public Layer getRootLayer() {
+        return getRootBox() == null ? null : getRootBox().getLayer();
+	}
+	    
+	public Box getRootBox() {
+	    return rootBox;
+	}
+
+	private void setRootBox(Box rootBox) {
+	    this.rootBox = rootBox;
+	}
+
+	public int getLayoutHeight() {
+		return getRootBox().getHeight();
+	}
+}
--- flying-saucer-examples/src/main/java/com/github/neoflyingsaucer/other/PDFRenderer.java
+++ flying-saucer-examples/src/main/java/com/github/neoflyingsaucer/other/PDFRenderer.java
@@ -1,10 +1,14 @@
 package com.github.neoflyingsaucer.other;
 
-import com.github.neoflyingsaucer.defaultuseragent.DefaultUserAgent;
 import com.github.neoflyingsaucer.extend.output.DisplayList;
 import com.github.neoflyingsaucer.extend.useragent.UserAgentCallback;
+import com.github.neoflyingsaucer.j2dout.Java2DFontContext;
+import com.github.neoflyingsaucer.j2dout.Java2DFontResolver;
+import com.github.neoflyingsaucer.j2dout.Java2DImageResolver;
 import com.github.neoflyingsaucer.j2dout.Java2DOut;
+import com.github.neoflyingsaucer.j2dout.Java2DReplacedElementResolver;
 import com.github.neoflyingsaucer.pdfout.PdfRenderer;
+
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -12,10 +16,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
-import java.util.HashMap;
-
 import javax.imageio.ImageIO;
+
+import org.xhtmlrenderer.renderers.ContinuousRenderer;
 
 /**
  * <p/>
@@ -54,13 +57,6 @@ public class PDFRenderer {
     	renderer.setDocument(url);  
     	doRenderToPDF(renderer, pdf);
     	
-		String html =
-				"<html><head><style>" +
-				"@page { size: 200px 400px; margin: 1px; }" +
-				"body { background-color: #f00; margin: 2px; border: 3px solid blue; height: 100px; border-radius: 8px; color: white; padding: 10px; }" +
-				"</style></head><body>TEST TEST LONG TEST LONG TEST LONG TEST LONG TEST LONG TEST LONG TEST LONG TEST</body></html>";
-    	
-    	
     	// Document doc = HTMLResourceHelper.load(html).getDocument();
     	DisplayList dl = DisplayListRenderer.renderToList(url, 1000, 1000, uac, 0);
     	System.err.println(dl.toString());
@@ -75,8 +71,45 @@ public class PDFRenderer {
     	g2d.dispose();
     	
     	ImageIO.write(img, "png", new File(pdf + ".img.png")); 
+    	
+    	renderToContinuousImage(url, uac, pdf + ".imgc.png");
     }
 
+    private static void renderToContinuousImage(String url, UserAgentCallback uac, String filename) throws IOException
+    {
+    	BufferedImage layoutGraphics = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+ 
+    	Graphics2D g2d2 = layoutGraphics.createGraphics();
+        g2d2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+    	
+    	ContinuousRenderer r3 = new ContinuousRenderer(uac);
+    	
+    	r3.setDocumentUri(url);
+    	r3.setImageResolver(new Java2DImageResolver());
+    	r3.setFontContext(new Java2DFontContext(g2d2));
+    	r3.setFontResolver(new Java2DFontResolver());
+    	r3.setReplacedElementResolver(new Java2DReplacedElementResolver());
+    	r3.setViewportSize(1000, 1000);
+    	
+    	DisplayList dl = r3.renderToList();
+    	int height = r3.getLayoutHeight();
+    	
+    	BufferedImage img = new BufferedImage(1000, height, BufferedImage.TYPE_INT_ARGB);
+    	
+    	Graphics2D g2d = img.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+  	
+    	Java2DOut out = new Java2DOut(g2d, RenderingHints.VALUE_ANTIALIAS_ON);
+    	out.render(dl);
+    	g2d.dispose();
+    	g2d2.dispose();
+    	
+    	ImageIO.write(img, "png", new File(filename)); 
+    }
+    
+    
     private static void doRenderToPDF(PdfRenderer renderer, String pdf) throws IOException {
         OutputStream os = null;
         try {
@@ -97,16 +130,4 @@ public class PDFRenderer {
         }
 		
 	}
-
-    /** prints out usage information, with optional error message
-     * @param err
-     */
-    private static void usage(final String err) {
-        if (err != null && err.length() > 0) {
-            System.err.println("==>" + err);
-        }
-        System.err.println("Usage: ... url pdf [version]");
-        System.err.println("   where version (optional) is between 1.2 and 1.7");
-        System.exit(1);
-    }
 }
