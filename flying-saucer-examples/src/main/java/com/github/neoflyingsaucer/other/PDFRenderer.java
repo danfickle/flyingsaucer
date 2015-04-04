af822066b60e613b7bd58cf89c6f214a586b3703
Dan Fickle <newtab@gmail.com>
2015-04-04 18:46:19 +1100

    Finished up removing dependencies on old pdf-out module.

--- flying-saucer-examples/src/main/java/com/github/neoflyingsaucer/other/PDFRenderer.java
+++ flying-saucer-examples/src/main/java/com/github/neoflyingsaucer/other/PDFRenderer.java
@@ -12,7 +12,6 @@ import com.github.neoflyingsaucer.pdf2dout.Pdf2FontResolver;
 import com.github.neoflyingsaucer.pdf2dout.Pdf2ImageResolver;
 import com.github.neoflyingsaucer.pdf2dout.Pdf2Out;
 import com.github.neoflyingsaucer.pdf2dout.Pdf2ReplacedElementResolver;
-import com.github.neoflyingsaucer.pdfout.PdfRenderer;
 
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -21,7 +20,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
